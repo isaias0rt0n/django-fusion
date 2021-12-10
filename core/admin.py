69ca3290cdf27c824067fc7617541ff4396080a2
isaiasorton <isaiasorton@gmail.com>
2021-12-09 22:05:15 -0300

    versão runtime finalizada

---
+++ core/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import *
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+
+@admin.register(Servico)
+class Servico(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
+
+@admin.register(Recurso)
+class RecursoAdmin(admin.ModelAdmin):
+    list_display = ('recurso', 'icone', 'ativo', 'modificado')
