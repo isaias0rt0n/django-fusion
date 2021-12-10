9a3becc8229f191c51500ca48554a1cacbe9ecc0
isaias0rt0n <isaiasorton@gmail.com>
2021-12-10 19:33:34 -0300

    admin-lte2

--- fusion/settings.py
+++ fusion/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core'
+    'django_adminlte',
+    'django_adminlte_theme',
+
+    'core',
 ]
 
 MIDDLEWARE = [
